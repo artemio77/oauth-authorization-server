76a2d3bc7cb90b37f82ba736d33221b155c735e1
Artem <derevets.artem@gmail.com>
2018-12-16 12:38:24 +0200

    travis/heroku test

--- src/main/java/com/derevets/artem/email/MailProdConfig.java
+++ src/main/java/com/derevets/artem/email/MailProdConfig.java
@@ -2,16 +2,12 @@ package com.derevets.artem.email;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
 @Configuration
-@Profile("release")
-@PropertySource("classpath:application-release.properties")
 public class MailProdConfig extends MailConfig {
 
 
