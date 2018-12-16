44a54bb83dfcef99f65b6ff7b5ddc33813f23efc
Artem <derevets.artem@gmail.com>
2018-12-16 03:46:02 +0200

    travis/heroku test

--- src/main/java/com/derevets/artem/config/MailConfig.java
+++ src/main/java/com/derevets/artem/config/MailConfig.java
@@ -1,7 +1,6 @@
 package com.derevets.artem.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -11,7 +10,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
 
 @Configuration
-@ConfigurationProperties("server")
+@PropertySource("classpath:application.properties")
 public class MailConfig {
 
     @Value("${mail.protocol}")
