4d08ef32dfd842a680c2102ee991dd631d484bc5
Artem <derevets.artem@gmail.com>
2019-01-06 20:11:49 +0200

    clean properties

--- src/main/java/com/derevets/artem/AuthorizationServerApplication.java
+++ src/main/java/com/derevets/artem/AuthorizationServerApplication.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @SpringBootApplication
 @ComponentScan("com.derevets.artem")
+@EnableJpaRepositories(basePackages = {
+        "com.derevets.artem"
+})
+@EnableTransactionManagement
 public class AuthorizationServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthorizationServerApplication.class, args);
