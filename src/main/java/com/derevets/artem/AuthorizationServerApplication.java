22de9d4b59bcb542a3c91c0644cea5e94108d904
Artem <derevets.artem@gmail.com>
2019-01-09 15:23:18 +0200

    rabbit init

--- src/main/java/com/derevets/artem/AuthorizationServerApplication.java
+++ src/main/java/com/derevets/artem/AuthorizationServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -15,6 +16,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
         "com.derevets.artem.repository"
 })
 @EnableTransactionManagement
+@PropertySource(value = {"classpath:application.properties",
+        "classpath:application-release.properties"})
 public class AuthorizationServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthorizationServerApplication.class, args);
