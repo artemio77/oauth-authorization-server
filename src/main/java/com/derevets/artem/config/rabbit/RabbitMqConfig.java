e04bd236005798f7da0be07c994169a532a20adc
Artem <derevets.artem@gmail.com>
2019-01-09 14:30:32 +0200

    rabbit init

--- src/main/java/com/derevets/artem/config/rabbit/RabbitMqConfig.java
+++ src/main/java/com/derevets/artem/config/rabbit/RabbitMqConfig.java
@@ -1,50 +1,29 @@
 package com.derevets.artem.config.rabbit;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.amqp.core.AmqpAdmin;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 
 
 @Slf4j
 @Configuration
-@PropertySource(value = {"classpath:application-dev.properties",
-        "classpath:application-release.properties"})
 public class RabbitMqConfig {
 
-    @Value("${rabbitmq.url}")
-    private String rabbitMqUrl;
+    @Autowired
+    private ConnectionFactory connectionFactory;
 
     @Bean
-    public ConnectionFactory connectionFactory() throws URISyntaxException {
-        URI uri = new URI(rabbitMqUrl);
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(uri);
-        if (StringUtils.isNotBlank(uri.getPath()))
-            connectionFactory.setVirtualHost(uri.getPath().replace("/", ""));
-        connectionFactory.setConnectionTimeout(3000);
-        connectionFactory.setRequestedHeartBeat(30);
-        return connectionFactory;
+    public AmqpAdmin amqpAdmin() {
+        return new RabbitAdmin(connectionFactory);
     }
 
     @Bean
-    public AmqpAdmin amqpAdmin() throws URISyntaxException {
-        return new RabbitAdmin(connectionFactory());
-    }
-
-    @Bean
-    public RabbitTemplate rabbitTemplate() throws URISyntaxException {
-        return new RabbitTemplate(connectionFactory());
+    public RabbitTemplate rabbitTemplate() {
+        return new RabbitTemplate(connectionFactory);
     }
 }
